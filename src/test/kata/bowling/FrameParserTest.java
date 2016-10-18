d7e77372d48879f9a46d4374047a00f62174e0ed
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2016-10-18 14:53:45 +0200

    now the parser is able to parse a simple frame

--- src/test/kata/bowling/FrameParserTest.java
+++ src/test/kata/bowling/FrameParserTest.java
@@ -16,13 +16,53 @@ public class FrameParserTest {
         assertThat(new FrameParser().parse(""), is(new ArrayList<Frame>()));
     }
 
+    @Test
+    public void a_single_frame() throws Exception {
+        List<Frame> frames = new FrameParser().parse("1-");
+        Frame scoreFrame = new ScoreFrame(1);
+
+        assertThat(frames.size(), is(1));
+        assertThat(frames.get(0), is(scoreFrame));
+        assertThat(frames.get(0).getScore(), is(1));
+    }
+
     private class FrameParser {
         List<Frame> parse(String frames) {
-            return new ArrayList<>();
+            ArrayList<Frame> frames1 = new ArrayList<>();
+            if (frames != null && !frames.isEmpty()) {
+                ScoreFrame frame = new ScoreFrame(1);
+                frames1.add(frame);
+            }
+            return frames1;
         }
     }
 
-    private class Frame {
-
+    private interface Frame {
+        int getScore();
     }
+
+    private class ScoreFrame implements Frame {
+
+        private int score;
+
+        ScoreFrame(int score) {
+            this.score = score;
+        }
+
+        @Override
+        public int getScore() {
+            return score;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            ScoreFrame frame = (ScoreFrame) o;
+
+            return score == frame.score;
+        }
+    }
+
 }
