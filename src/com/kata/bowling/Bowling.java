f7bca5df921279f6abe94057935e7dc847373533
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2016-10-21 15:44:46 +0200

    now we are restarting to create bowling class

--- src/com/kata/bowling/Bowling.java
+++ src/com/kata/bowling/Bowling.java
@@ -1,15 +1,14 @@
 package com.kata.bowling;
 
-import java.util.List;
-
 public class Bowling {
 
     public Bowling() {
     }
 
-    public int total(List<Frame> frames) {
-        return frames.get(0).getScore();
-    }
+//    public int total(List<Frame> frames) {
+//        return
+//        return frames.get(0).getScore();
+//    }
 
     public static void main(String[] args) {
         // write your code here
--- src/test/kata/bowling/BowlingTest.java
+++ src/test/kata/bowling/BowlingTest.java
@@ -1,43 +1,43 @@
 package test.kata.bowling;
 
-import com.kata.bowling.*;
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
 public class BowlingTest {
 
-    @Test
-    public void a_single_frame() {
-        List<Frame> frames = new ArrayList<>();
-        ScoreFrame frame = new ScoreFrame(4, 0);
-        frames.add(frame);
-        Bowling bowling = new Bowling();
-        assertThat(bowling.total(frames), is(frame.getScore()));
-    }
-
-    @Test
-    public void a_single_spare_frame() {
-        List<Frame> frames = new ArrayList<>();
-        Frame frame = new SpareFrame(4);
-        frames.add(frame);
-        Bowling bowling = new Bowling();
-        assertThat(bowling.total(frames), is(frame.getScore()));
-    }
-
-    @Test
-    public void a_single_strike_frame() {
-        List<Frame> frames = new ArrayList<>();
-        Frame frame = new StrikeFrame();
-        frames.add(frame);
-        Bowling bowling = new Bowling();
-        assertThat(bowling.total(frames), is(frame.getScore()));
-    }
-
-
+//    @Test
+//    public void a_single_frame() {
+//        List<Frame> frames = new ArrayList<>();
+//        ScoreFrame frame = new ScoreFrame(4, 0);
+//        frames.add(frame);
+//        Bowling bowling = new Bowling();
+//        assertThat(bowling.total(frames), is(frame.getScore()));
+//    }
+//
+//    @Test
+//    public void a_single_spare_frame() {
+//        List<Frame> frames = new ArrayList<>();
+//        Frame frame = new SpareFrame(4);
+//        frames.add(frame);
+//        Bowling bowling = new Bowling();
+//        assertThat(bowling.total(frames), is(frame.getScore()));
+//    }
+//
+//    @Test
+//    public void a_single_strike_frame() {
+//        List<Frame> frames = new ArrayList<>();
+//        Frame frame = new StrikeFrame();
+//        frames.add(frame);
+//        Bowling bowling = new Bowling();
+//        assertThat(bowling.total(frames), is(frame.getScore()));
+//    }
+//
+//    @Test
+//    public void two_frames_without_strikes_or_spares() throws Exception {
+//        List<Frame> frames = new ArrayList<>();
+//        ScoreFrame frame_1 = new ScoreFrame(2, 0);
+//        ScoreFrame frame_2 = new ScoreFrame(4, 3);
+//        frames.add(frame_1);
+//        frames.add(frame_2);
+//        Bowling bowling = new Bowling();
+//        assertThat(bowling.total(frames), is(frame_1.getScore() + frame_2.getScore()));
+//    }
 
 }
