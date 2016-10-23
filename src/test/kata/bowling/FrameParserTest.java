7d720414d901ffdcf541510bbe0a8ead62f30be2
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2016-10-23 15:21:24 +0200

    now the parser has benn extracted

--- src/test/kata/bowling/FrameParserTest.java
+++ src/test/kata/bowling/FrameParserTest.java
@@ -15,7 +15,7 @@ public class FrameParserTest {
 
     @Test
     public void no_frames() throws Exception {
-        assertThat(new FrameParser().parse((String) null), is(new ArrayList<Frame>()));
+        assertThat(new FrameParser().parse(null), is(new ArrayList<Frame>()));
         assertThat(new FrameParser().parse(""), is(new ArrayList<Frame>()));
     }
 
@@ -67,7 +67,7 @@ public class FrameParserTest {
         assertThat(frames.get(1).getScore(), is(5));
     }
 
-    @Test(expected = FrameParseException.class)
+    @Test(expected = FrameParser.FrameParseException.class)
     public void five_trials_without_strikes() throws Exception {
         List<Frame> frames = new FrameParser().parse("4--5-");
 
@@ -135,112 +135,6 @@ public class FrameParserTest {
         assertThat(frames.get(0), is(frame_1));
         assertThat(frames.get(1), is(frame_2));
         assertThat(frames.get(2), is(frame_3));
-    }
-
-    private class FrameParser {
-
-        private static final int STRIKE = -1;
-        private static final int SPARE = -2;
-
-        List<Frame> parse(String frameString) {
-            ArrayList<Frame> frames = new ArrayList<>();
-            if (frameString != null && !frameString.isEmpty()) {
-                int[] trialScores = getSingleTrialScores(frameString);
-                frames = parse(trialScores);
-                updateFrameStructure(frames);
-            }
-            return frames;
-        }
-
-        private void updateFrameStructure(ArrayList<Frame> frames) {
-            for (int i = 0; i < frames.size(); i++) {
-                Frame frame = getFrameAtIndex(frames, i);
-                Frame nextFrame = getFrameAtIndex(frames, i + 1);
-                nextFrame.add(getFrameAtIndex(frames, i + 2));
-                frame.add(nextFrame);
-            }
-        }
-
-        private Frame getFrameAtIndex(ArrayList<Frame> frames, int i) {
-            Frame frame;
-            if (i < frames.size()) {
-                frame = frames.get(i);
-            }
-            else {
-                frame = new NullFrame();
-            }
-            return frame;
-        }
-
-        private ArrayList<Frame> parse(int[] trialScores) {
-            return parse(0, trialScores);
-        }
-
-        private ArrayList<Frame> parse(int numberOfTrialsOffset, int[] trialScores) {
-            ArrayList<Frame> frames = new ArrayList<>();
-            if (numberOfTrialsOffset >= trialScores.length) {
-                return frames;
-            } else {
-                int numberOfTrialsOfNextFrame = getNextFrameIndex(numberOfTrialsOffset, trialScores);
-                Frame frame = parseFrame(numberOfTrialsOffset, numberOfTrialsOfNextFrame, trialScores);
-                frames.add(frame);
-                frames.addAll(parse(numberOfTrialsOffset + numberOfTrialsOfNextFrame, trialScores));
-                return frames;
-            }
-        }
-
-        private int getNextFrameIndex(int indexOffset, int[] trialScores) {
-            int nextFrameIndex = 2;
-            if (trialScores[indexOffset] == STRIKE) {
-                nextFrameIndex = 1;
-            }
-            return nextFrameIndex;
-        }
-
-        private Frame parseFrame(int numberOfTrialsOffset, int nextFrameIndex, int[] trialScores) {
-            Frame frame;
-            if (nextFrameIndex == 1) {
-                frame = new StrikeFrame();
-            } else if (numberOfTrialsOffset + 1 < trialScores.length) {
-                int firstTrialScore = trialScores[numberOfTrialsOffset];
-                int secondTrialScore = trialScores[numberOfTrialsOffset + 1];
-                if (secondTrialScore == SPARE) {
-                    frame = new SpareFrame(firstTrialScore);
-                } else {
-                    frame = new ScoreFrame(firstTrialScore, secondTrialScore);
-                }
-            } else {
-                throw new FrameParseException();
-            }
-            return frame;
-        }
-
-        private int[] getSingleTrialScores(String frameString) {
-            char[] charArrays = frameString.toCharArray();
-            int[] trialScores = new int[charArrays.length];
-            for (int i = 0; i < trialScores.length; i++) {
-                trialScores[i] = parseSingleTrialScore(charArrays[i]);
-            }
-            return trialScores;
-        }
-
-        private int parseSingleTrialScore(char stringValue) {
-            if (stringValue >= '1' && stringValue <= '9') {
-                return Character.getNumericValue(stringValue);
-            } else if (stringValue == 'X') {
-                return STRIKE;
-            } else if (stringValue == '/') {
-                return SPARE;
-            } else if (stringValue == '-') {
-                return 0;
-            } else {
-                throw new FrameParseException();
-            }
-        }
-
-    }
-
-    private class FrameParseException extends RuntimeException {
     }
 
 }
